f89bfa94309eec4870fbf8390761c18ab667ac2b
Dylan <delente.dylan@gmail.com>
2025-09-11 14:13:44 +0200

    creat all logo for all games, del PNG, add dis steup all games

--- src/pages/RulesGame.js
+++ src/pages/RulesGame.js
@@ -1,4 +1,9 @@
 const RulesGame = ({ game }) => {
+    function formatPlayers(players) {
+            if (!players || players.length === 0) return "";
+            if (players.length === 1) return `${players[0]} joueurs`;
+        return `entre ${players[0]} et ${players[1]} joueurs`;
+    }
 
     if (!game) {
         return (
@@ -12,23 +17,47 @@ const RulesGame = ({ game }) => {
             <h2>{game.name}</h2>
             <p>{game.description}</p>
             <h3>setup :</h3>
-            <ul>
+            <div className="game-setup">
+                <div className="game-material">
+                    {game.material.map((material, index)=> (
+                    <div key={index}>
+                        <p>{material.name} x{material.count} ({material.effect}) </p>
+                        <img src={material.img} alt="objet" height={30}/>
+                    {index < game.material.length - 1 ? '' : ''}</div>
+                ))}                    
+                </div>
+                <div className="game-players">
+                    <div>
+                        <p>{formatPlayers(game.playeurs)}</p>
+                        <img src= {game.img} alt="joueurs" height={30} />
+                    </div>
+                </div>
+                <div className="game-items">
                 {game.setup.map((setup, index) => (
-                    <p key={index}> {setup.name} {setup.value}</p>
+                    <div key={index}>
+                        <p> {setup.name} {setup.value}</p>
+                        <img src={setup.img} alt="objet nécessaire" height={30}/>
+                    {index < game.setup.length - 1 ? '' : ''}</div>
                 ))}
-            </ul>
+                </div>
+                
+            </div>
             <h3>Règles du jeu :</h3>
-            <ul>
-                {game.rules.map((rule, index) => (
-                    <p key={index}>{rule}</p>
-                ))}
-            </ul>
-            <h3>Etapes du jeu :</h3>
-            <ul>
-                {game.steps.map((steps, index) => (
-                    <p key={index}>{steps}</p>
-                ))}
-            </ul>
+            <div className="game-rule">
+                <p>
+                    {game.rules.map((rule, index) => (
+                        <p key={index}>{rule}</p>
+                    ))}
+                </p>
+            </div>
+            <h3>Etapes du jeu :</h3> 
+            <div className="game-step">   
+                <p>
+                    {game.steps.map((steps, index) => (
+                        <p key={index}>{steps}</p>
+                    ))}
+                </p>
+            </div>
         </div>
     )
 }
